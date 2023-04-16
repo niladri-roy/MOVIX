14a5cdd8c539164a75bec990f7ce8caf7d95041a
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-04-16 23:43:59 +0530

    ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState , useEffect} from 'react'
+import {fetchDataFromApi} from "./utils/api"
+import {useSelector, useDispatch} from 'react-redux'
+import {getApiConfiguration} from "./store/homeSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const dispatch = useDispatch();
+  const {url} = useSelector((state) => 
+    state.home
+  )
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular')
+      .then((res) => {
+        console.log(res);
+        dispatch(getApiConfiguration(res));
+      });
+  };
 
   return (
     <div className="App">
+      App
+      {url?.total_pages}
     </div>
   )
 }
